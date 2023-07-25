7bc5f3bfe67bef5377d1245e2e9f645936226a60
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2023-07-25 15:41:00 +0300

    Update custom_printf.c first try

--- custom_printf.c
+++ custom_printf.c
@@ -1,2 +1,50 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    int charCount = 0;
+
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    charCount += putchar(va_arg(args, int));
+                    break;
+                case 's': {
+                    const char *str = va_arg(args, const char *);
+                    while (*str != '\0') {
+                        charCount += putchar(*str);
+                        str++;
+                    }
+                    break;
+                }
+                case '%':
+                    charCount += putchar('%');
+                    break;
+                default:
+                    charCount += putchar('%');
+                    charCount += putchar(*format);
+            }
+        } else {
+            charCount += putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+    return charCount;
+}
+
+int main() {
+    int charCount = _printf("Printing characters: %c %c %c\n", 'A', 'B', 'C');
+    charCount += _printf("Printing a string: %s\n", "Hello, World!");
+    charCount += _printf("Printing a percentage sign: %%\n");
+    printf("Total characters printed: %d\n", charCount);
+    return 0;
+}
+
