0793b84a94e347a1bd5c4967f56219615c152502
khanyi2004 <khanyi.ramo@gmail.com>
2023-07-26 03:19:29 -0700

    printf

--- _printf.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-/**
- * _printf -is a function that selects the correct function to print.
- * @format : identifier to look for.
- * Return : Printed chars.
- */ 
-int _printf(const char *format, ...);
-
