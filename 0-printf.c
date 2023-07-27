e1e8f698aef6413695f63302972012ad32e52b72
Teresia Kirung’o <40103113+Teresia-Kirungo@users.noreply.github.com>
2023-07-27 10:09:41 +0300

    Update 0-printf.c a function that produces output according to a format.

--- 0-printf.c
+++ 0-printf.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
 #include "main.h"
+
+/**
+ * _printf - function my printf
+ * @format: string with format to print
+ *
+ * Return: number of chars that print
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int length_of_string = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	length_of_string = _print_format(format, args);
+	va_end(args);
+	return (length_of_string);
+}
